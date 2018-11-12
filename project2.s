72f45a5ebd897cfce599b734cc5c53462e65633b
Genesis <genesis.smothers@bison.howard.edu>
2018-11-12 14:35:27 -0500

    I allcoated 4 bytes of memory for the string that the user gives and then i gave the instruction for the program to expect userinput

--- project2.s
+++ project2.s
@@ -1,8 +1,10 @@
 .data
-
+    str: .space 4
+    
 .text 
     main: 
-    
+    #tells program to expect userinput
+    li $v0, 8 
     #tell the system the end of main 
     li $v0, 10
     syscall
