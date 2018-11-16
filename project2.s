c567dd63c7fd9e19abe12ccef6276dbdd5694cfe
Genesis <genesis.smothers@bison.howard.edu>
2018-11-16 16:36:50 -0500

    i allocated 1000 spaces instead of 4 because of testing with spaces and i added messages for base number and empty input.
    I allocated 1000 spaces instead of 4 because of testing with spaces and i added messages for base number and empty input.

--- project2.s
+++ project2.s
@@ -1,12 +1,13 @@
 .data
-    str: .space 4
-    
+    str: .space 1000
+    msgempty: .asciiz "Input is empty"
+    msginvalid: .asciiz "Invalid base-28 number"
 .text 
     main: 
     #tells program to expect userinput
     li $v0, 8 
     la $a0, str
-    li $a1, 4
+    li $a1, 1000
     syscall 
     #tell the system the end of main 
     li $v0, 10
